46310c52e2418b6f3e244fa82a10896d04223bbf
machete <rtpppp163@gmail.com>
2020-06-06 04:17:17 +0800

    fix bug

--- bin/main.js
+++ bin/main.js
@@ -26,22 +26,22 @@ function wirteToFile () {
 function main (options) {
   setColors(options.colors)
   setConfig(options)
-  console.time();
+  console.time('初始化耗时');
   filterClassNames(getAllVueFileClassStr())
   wirteToFile()
   console.log('=============初始化完成=============')
-  console.timeEnd('初始化耗时')
+  console.timeEnd()
   console.log('\n\n')
   const watcher = chokidar.watch(path.resolve(getConfig(DIR_PATH)), {
     ignored: new RegExp(`^.*\\.(?:(?!(${getConfig(EXT_NAME)})).)+$`),
     persistent: true
   })
   watcher.on('change', () => {
-    console.time();
+    console.time('热更新耗时');
     filterClassNames(getAllVueFileClassStr())
     wirteToFile()
     console.log('=============热更新完成=============')
-    console.timeEnd('热更新耗时')
+    console.timeEnd()
     console.log('\n\n')
   })
 }
