4cf174b867284cd741eb9effd1e8192ae3f95b19
虎哥 <erpppp163@gmail.com>
2020-06-30 15:37:33 +0800

    插件 v0.12.1 border-top 会有兼容问题  改为 0 0 0 0

--- src/rules/border.js
+++ src/rules/border.js
@@ -1,31 +1,9 @@
 /**
  * order 520 460 + 60
  */
-const { UNIT_ENMU_STR, DIRECTION_MAP } = require('../constant')
+const { UNIT_ENMU_STR } = require('../constant')
 const { getUnit } = require('../config')
 
-function getOrder (direction) {
-  let order = 460
-  if (direction === 'x') {
-    order += 10
-  }
-  if (direction === 'y') {
-    order += 20
-  }
-  if (direction === 't') {
-    order += 30
-  }
-  if (direction === 'b') {
-    order += 40
-  }
-  if (direction === 'r') {
-    order += 50
-  }
-  if (direction === 'l') {
-    order += 60
-  }
-  return order
-}
 module.exports = {
   regExp: new RegExp(`^(border|border-width|border-w)-((?<direction>[trblxy])-)?(?<num>0|[1-9]\\d*)(?<unit>${UNIT_ENMU_STR})?$`),
   render ({ groups }) {
@@ -35,14 +13,38 @@ module.exports = {
     } else {
       unit = getUnit(unit)
     }
-    return {
-      name: 'border',
-      order: getOrder(direction),
-      num,
-      css: DIRECTION_MAP
-        .get(direction)
-        .reduce((t, c) => [c ? `border-${c}-width: ${num}${unit}` : `border-width: ${num}${unit}`, ...t], ['border-style: solid', 'border-color: #000'])
-
+    let order = 460
+    let numStr = ''
+    const val = `${num}${unit}`
+    if (direction === 'x') {
+      order += 10
+      numStr = `0 ${val}`
     }
+    if (direction === 'y') {
+      order += 20
+      numStr = `${val} 0`
+    }
+    if (direction === 't') {
+      order += 30
+    }
+    numStr = `${val} 0 0 0`
+    if (direction === 'r') {
+      order += 50
+      numStr = `0 ${val} 0 0`
+    }
+    if (direction === 'b') {
+      order += 40
+      numStr = `0 0 ${val} 0`
+    }
+    if (direction === 'l') {
+      numStr = `0 0 0 ${val}`
+      order += 60
+    }
+    if (direction === undefined) {
+      order += 0
+      numStr = `${val}`
+    }
+    const base = { name: 'border', order, num }
+    return { ...base, css: [`border-width: ${numStr}`, 'border-style: solid', 'border-color: #000'] }
   }
 }
