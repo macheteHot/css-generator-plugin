5bfe525a963aacb8d936e65c4bcb2442e79e044b
gugu <252819252@qq.com>
2020-07-08 15:23:08 +0800

    fix flex basis bug

--- src/rules/flexBasis.js
+++ src/rules/flexBasis.js
@@ -2,11 +2,16 @@
  * order 280
  */
 const { UNIT_ENMU_STR } = require('../constant')
+const { getUnit } = require('../config')
 
 module.exports = {
-  regExp: new RegExp(`^flex-basis-(?<value>(0|[1-9]\\d*(${UNIT_ENMU_STR}))|initial|inherit|auto)$`),
+  regExp: new RegExp(`^flex-basis-(?<value>((?<num>0|[1-9]\\d*)(?<unit>${UNIT_ENMU_STR})?)|initial|inherit|auto)$`),
   render ({ groups }) {
-    const { value } = groups
+    let { value, num, unit } = groups
+    if (num) {
+      unit = getUnit(unit)
+      value = `${num}${unit}`
+    }
     return {
       name: 'flexBasis',
       order: 280,
