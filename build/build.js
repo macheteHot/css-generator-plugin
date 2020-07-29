27a31df059ed88d735c8fb0af032a4a150bef98b
虎哥 <erpppp163@gmail.com>
2020-07-29 13:40:11 +0800

    change build and fix docs file bug

---
+++ build/build.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack')
+const path = require('path')
+const fs = require('fs')
+
+function resolve (...args) {
+  return path.resolve(__dirname, ...args)
+}
+
+const output = {
+  path: resolve('../dist'),
+  filename: 'gcss.js'
+}
+
+const config = {
+  mode: 'production',
+  entry: resolve('../src/scriptUse.js'),
+  devtool: 'none',
+  output
+}
+
+webpack(config, (err) => {
+  if (err) {
+    console.error(err)
+  } else {
+    fs.copyFileSync(resolve(output.path, output.filename), resolve(resolve('../docs/', output.filename)))
+  }
+})
